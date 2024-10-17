b675eab7b9af6f2494f3da4bc24a3ba5d522f0fe
Cris272500 <cristopherquincer@gmail.com>
2024-10-17 02:25:17 -0600

    Informacion extra
    Se agrega mas informacion de la hoja de identifiacion, osea mas options en los select

--- medicapp/src/components/record/HIdentificacion.js
+++ medicapp/src/components/record/HIdentificacion.js
@@ -12,9 +12,15 @@ const HIdentificacion = () => {
                 </div>
                 {/* Aqui pondria la imagen del logo de medicina */}
             </div>
+            
+            <div className="expediente-header">
+                <div className="fecha-expediente">
+                    <span id="fechaActual">sábado, 19 de septiembre de 2024 </span>
+                </div>
 
-            <div className="expediente">
-                <span>Expediente: 29294ASD</span>
+                <div className="expediente">
+                    <span>Expediente: 29294ASD</span>
+                </div>
             </div>
 
 
@@ -70,19 +76,65 @@ const HIdentificacion = () => {
                 <div className="section full-row">
                     <h3>Enfermedades Infecto - Contagiosas:</h3>
                     <div className="checkbox-group">
-                    <label><input type="checkbox"/>Hepatitis</label>
-                    <label><input type="checkbox"/>Sífilis</label>
-                    <label><input type="checkbox"/>TB</label>
-                    <label><input type="checkbox"/>Cólera</label>
+                        <label className="checkbox-label"><input type="checkbox"/>Hepatitis</label>
+                        <label className="checkbox-label"><input type="checkbox"/>Sífilis</label>
+                        <label className="checkbox-label"><input type="checkbox"/>TB</label>
+                        <label className="checkbox-label"><input type="checkbox"/>Amebiasis</label>
+                        <label className="checkbox-label"><input type="checkbox"/>Tosferina</label>
+                        <label className="checkbox-label"><input type="checkbox"/>Sarampion</label>
+
+                        <label className="checkbox-label"><input type="checkbox"/>Meningitis</label>
+                        <label className="checkbox-label"><input type="checkbox"/>Rubéola</label>
+                        <label className="checkbox-label"><input type="checkbox"/>Parotiditis</label>
+                        <label className="checkbox-label"><input type="checkbox"/>Impétigo</label>
+                        <label className="checkbox-label"><input type="checkbox"/>Fiebre tifoidea</label>
+                        <label className="checkbox-label"><input type="checkbox"/>Escarlatina</label>
+                        <label className="checkbox-label"><input type="checkbox"/>Malaria</label>
+
+                        <label className="checkbox-label"><input type="checkbox"/>Escabiosis</label>
+                        <label className="checkbox-label"><input type="checkbox"/>Pediculosis</label>
+                        <label className="checkbox-label"><input type="checkbox"/>Tiña</label>
+
+
+                         <label className="checkbox-label">
+                            <input type="checkbox"/>Otros
+                        </label>
+                        <textarea className="textarea-otros" placeholder="Especifique otros..."></textarea>
+
+
                     </div>
                 </div>
 
                 <div className="section full-row">
                     <h3>Enfermedades hereditarias:</h3>
-                    <div className="checkbox-group">
-                    <label><input type="checkbox"/>Alergias</label>
-                    <label><input type="checkbox"/>Diabetes mellitus</label>
-                    <label><input type="checkbox"/>Hipertensión arterial</label>
+                    <div className="checkbox-group-hereditarias">
+                        <label><input type="checkbox"/>Alergias</label>
+                        <label><input type="checkbox"/>Diabetes mellitus</label>
+                        <label><input type="checkbox"/>Hipertensión arterial</label>
+                        <label><input type="checkbox"/>Enfermedad reumática</label>
+
+                        <label><input type="checkbox"/>Enfermedades renales</label>
+                        <label><input type="checkbox"/>Enfermedades oculares</label>
+                        <label ><input type="checkbox"/>Enfermedades cardiacas</label>
+
+
+                        <label><input type="checkbox"/>Enfermedad hepática</label>
+                        <label><input type="checkbox"/>Enfermedades musculares</label>
+                        <label><input type="checkbox"/>Malformaciones congénitas</label>
+
+                        <label><input type="checkbox"/>Desórdenes mentales</label>
+                        <label><input type="checkbox"/>Enfermedades degenerativas del sistema nervioso central</label>
+
+                        <label><input type="checkbox"/>Anomalías del crecimiento y desarrollo</label>
+                        <label><input type="checkbox"/>Errores innatos del metabolismo</label>
+                        <label><input type="checkbox"/>Otros</label>
+
+                        <textarea className="textarea-otros" placeholder="Especifique otros..."></textarea>
+
+
+
+
+
                     </div>
                 </div>
             </form>
